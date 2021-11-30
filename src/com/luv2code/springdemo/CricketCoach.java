440a6b429a6db62a4f52d052c547d3c4b0d93bd1
JiafanHe <jiafan453@gmail.com>
2021-11-30 10:52:27 -0500

    Edit spring config file for CricketCoach

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -1,17 +1,30 @@
 package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// create a no-arg constructor
+	public CricketCoach() {
+		
+	}
+	
+	
+	// our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Practice fast bowling for 15 minutes";
 	}
 
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
